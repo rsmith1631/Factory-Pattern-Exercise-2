6f804f28a2527b0cd64116606822a2329e2bfe42
rsmith1631 <rsmith1631@gmail.com>
2023-07-25 20:30:31 -0700

    Finished Factory Pattern exericise 2

--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput;
+            bool correctInput;
+            do {
+                Console.Clear();
+                correctInput = true;
+                Console.WriteLine("Which database would you like to use?");
+                Console.WriteLine("Type: List");
+                Console.WriteLine("Type: SQL");
+                Console.WriteLine("Type: Mongo");
+
+                userInput = Console.ReadLine();
+                if (userInput != "list" && userInput != "SQL" && userInput != "Mongo")
+                {
+                    correctInput = false;
+                    Console.WriteLine("Bad Input!");
+                    Thread.Sleep(1000);
+                }
+            } while (!correctInput);
+
+            Console.Clear ();
+
+            IDataAccess database = DataAccessFactory.GetDataAccessType(userInput);
+
+            database.SaveData();
+            var products = database.LoadData();
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Name: {product.Name} Price: {product.Price}");
+            }
+            
         }
     }
 }
